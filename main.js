7c28c74223584080c3afc71ae708bc6c343b1057
Blake Ford <blakeaford88@gmail.com>
2020-04-26 14:42:45 -0600

    added functionality

--- main.js
+++ main.js
@@ -1,14 +1,16 @@
 $(document).ready(function (){
+   //-----------------------CURRENT WEATHER--------------------------//
     $("#citysearch").click(function(){
-       
         displaySearchHistory ($("#searchinput").val())
         fiveDayForecast() 
         $.ajax({
             url: "https://api.openweathermap.org/data/2.5/weather?q="+$("#searchinput").val()+"&units=imperial&appid=94a9f51f96f39e938bcacd6bd7b28c53", success: 
             function(result){
+                console.log(result)
+    // cannot find the date
               $("#weatherdisplay").empty()
-
-              var nameDate = $("<h3>").text("Current Conditions:  " + result.name + " (4/23/20)" )
+   // needs current date to  v
+              var nameDate = $("<h3>").text("Current Conditions: " + result.name + " (current date)")
               $("#weatherdisplay").append(nameDate)
 
               var temp = $("<p>").text("Temperature: " + result.main.temp + " °F")
@@ -21,20 +23,21 @@ $(document).ready(function (){
               $("#weatherdisplay").append(wind)
                 
               uvi(result.coord.lat, result.coord.lon)
-
-        }
+// ----------------------GET WEATHER ICON TO SHOW IN THIS DIV FOR THE CURRENT WEATHER------------//
+            
+            //   var iconUrl = 'http://openweathermap.org/img/wn/'+forecast[i].weather[0].icon+'@2x.png'
+            //   var iconPic = $('<img>').attr('src',iconUrl)
+            //   $('#weatherdisplay').append(iconPic)
+            }
     });         
     })
-    
-    
+//-----------------------SEARCH HISTORY--------------------------//
     function displaySearchHistory (city){
-
         if (localStorage.getItem("history")) {
             var oldHistory = JSON.parse(localStorage.getItem("history"))
             oldHistory.push(city)
             var strSearchAry = JSON.stringify(oldHistory)
             localStorage.setItem("history", strSearchAry)  
-
         } else {
             var searchArray = []
             searchArray.push(city)
@@ -48,52 +51,54 @@ $(document).ready(function (){
           
             $(".cityDisplay").append(search)
         }
-
     }
-
+//-----------------------UVI INDEX--------------------------//
     function uvi (lat, lon){
                 $.ajax({
                     url:"http://api.openweathermap.org/data/2.5/uvi?appid=94a9f51f96f39e938bcacd6bd7b28c53&lat="+ lat + "&lon=" + lon
                 }).then(function(data){
                     var uvi = $("<p>").text("UV Index: " + data.value)
                     $("#weatherdisplay").append(uvi).addClass('uviBox')
-                    
                 })
 }
-
-           
-
-    function fiveDayForecast () {
+//----------------------5 DAY FORECAST----------------------//
+function fiveDayForecast () {
         $.ajax({
              url:"http://api.openweathermap.org/data/2.5/forecast?q="+$("#searchinput").val()+"&units=imperial&appid=94a9f51f96f39e938bcacd6bd7b28c53"
          }).then(function(data){
-        var forecast = [] 
+       
+            var forecast = [] 
         $('#forecastdisplay').empty()
         for (let index = 0; index < data.list.length; index++) {
             if (data.list[index].dt_txt.split(" ")[1] === "00:00:00"){
                 forecast.push(data.list[index])
+                // located date to append under iconPic
+                console.log(forecast[0])
             }
         }
-
         for (let i = 0; i < forecast.length; i++) {
-            
             var iconUrl = 'http://openweathermap.org/img/wn/'+forecast[i].weather[0].icon+'@2x.png'
+            
+            var displayForecastDate = $("<h3>").text(forecast[i].dt_txt.split(" ")[0])
+            $("#forecastdisplay").append(displayForecastDate)
+            // === "MMM DD YYYY"
             var iconPic = $('<img>').attr('src',iconUrl)
             $('#forecastdisplay').append(iconPic).addClass('iconPic')
             
-            // var iconDate = $('<p>').text('4/23/20')
-            // $('#forecastdisplay').append(iconDate)
+            console.log(forecast[0].main.temp)
+          
+            var forecastTemp = $('<p>').text("Temp:  " + forecast[0].main.temp + " °F")
+            $('#forecastdisplay').append(forecastTemp)
 
+            var forecastHumid = $('<p>').text("Humidity:  " + forecast[0].main.humidity + "%")
+            $('#forecastdisplay').append(forecastHumid)
         }
-        
     })
-
-    
     } 
-
 })
-            // var iconPic = $('<img>').attr('src',iconUrl)
-            // $('#forecastdisplay').append(iconPic).addClass('iconPic')
-            // var fiveDayTemp = $('<h3>').text(forecast[i].main.temp)
-            // $('#forecastdisplay').append(fiveDayTemp)
-
+// ------------------------------------UVI COLORS---------------------------------//
+// 0 to 2  = Green "Low"
+// 3 to 5  = Yellow "Moderate"
+// 6 to 7  = Orange "High"
+// 8 to 10 = Red "Very High"
+// 11+     = Violet "Extreme"
